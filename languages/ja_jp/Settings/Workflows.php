508afb20044d36b9305bb6615f968bdc8de05d17
Hideo Sugimoto <sugimoto@thinkingreed.co.jp>
2020-12-22 19:07:42 +0900

    refs #742 ワークフローの｢項目の値の更新｣アクションの表現式にget_specified_date関数を追加
    (cherry picked from commit 130d7dbb6753e89038eb90c372790698d49f603e)

--- languages/ja_jp/Settings/Workflows.php
+++ languages/ja_jp/Settings/Workflows.php
@@ -84,6 +84,14 @@ $languageStrings = array(
 	'LBL_NOTIFY_OWNER' => 'notify_owner',
 	'LBL_ANNUAL_REVENUE' => '年間売上',
 	'LBL_EXPRESSION_EXAMPLE2' => "if mailingcountry == 'India' then concat(firstname,' ',lastname) else concat(lastname,' ',firstname) end",
+	'LBL_EXPRESSION_EXAMPLE3' => "get_specified_date(datefield,'datelanguage')<br>
+		&emsp;datelanguage&emsp;ex:datefield=2020-01-01<br>
+		&emsp;-&emsp;first day of next month&emsp;->&emsp;2020-02-01<br>
+		&emsp;-&emsp;last day of next month&emsp;->&emsp;2020-02-28<br>
+		&emsp;-&emsp;first day of this month&emsp;->&emsp;2020-01-01<br>
+		&emsp;-&emsp;last day of this month&emsp;->&emsp;2020-01-31<br>
+		&emsp;-&emsp;first day of last month&emsp;->&emsp;2020-12-01<br>
+		&emsp;-&emsp;last day of last month&emsp;->&emsp;2020-12-31",
 	'LBL_FROM' => 'From',
 	'LBL_RUN_WORKFLOW' => 'ワークフローを実行',
 	'LBL_AT_TIME' => '時刻',
